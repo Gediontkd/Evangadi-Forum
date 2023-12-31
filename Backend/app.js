fd11f353cd69f9dbd2ed2cc442fa87e4172d5718
Gedion Daniel <gediondaniel321@gmail.com>
2023-12-31 09:47:11 +0100

    user routes added

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 5500
+// import the router
+const userRouter = require('./routes/userRoute.js');
 
-
+app.use("/api/users",userRouter);
 // const dbconnection = require("./db/dbConfig.js")
+
+
+
+
+
+
+
+app.listen(port, (err) =>{
+    if(err){
+    console.log(err.message)
+    } else {
+        console.log(`Server is running on ${port}`);
+    }
+});
